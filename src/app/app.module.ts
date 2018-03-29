0f6fc7c97b59769ecfee7cf372c2462b5fbaa195
Milad <miladfahimi2012@gmail.com>
2018-03-29 14:56:49 +0200

    Delete static component
    charts
    users
    products
    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,21 +13,13 @@ import { SubsidebarComponent } from './main/sidebar/subsidebar/subsidebar.compon
 import { ContentsComponent } from './main/contents/contents.component';
 import { ContentComponent } from './main/contents/content/content.component';
 import { NavComponent } from './nav/nav.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
-import { ProductComponent } from './product/product.component';
-import { ChartComponent } from './chart/chart.component';
 
 
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent, outlet: 'expandmenu' },
   { path: 'sidebar', component: SubsidebarComponent, outlet: 'expandmenu' },
   { path: 'content', component: ContentComponent },
   { path: '', redirectTo: '/content', pathMatch: 'full' },
-  { path: 'user', component: UserComponent, outlet: 'expandmenu' },
-  { path: 'product', component: ProductComponent, outlet: 'expandmenu' },
-  { path: 'chart', component: ChartComponent, outlet: 'expandmenu' },
-  { path: 'close', component: ChartComponent }
+  { path: 'chart', component: SubsidebarComponent, outlet: 'expandmenu' },
 ];
 
 
@@ -39,12 +31,8 @@ const appRoutes: Routes = [
     SubsidebarComponent,
     ContentsComponent,
     ContentComponent,
-    NavComponent,
-    HomeComponent,
-    UserComponent,
-    ProductComponent,
-    ChartComponent
-  ],
+    NavComponent
+    ],
   imports: [BrowserModule, FormsModule, HttpClientModule,
     RouterModule.forRoot(appRoutes)],
   providers: [
